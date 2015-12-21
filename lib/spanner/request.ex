137d6368210def0bc44226afe8a3b86c66294c8d
Christopher Maier <chris@operable.io>
2015-12-21 11:39:37 -0500

    Don't encode as JSON; just wrap in an envelope

--- lib/spanner/request.ex
+++ lib/spanner/request.ex
@@ -1,21 +1,11 @@
 defmodule Spanner.Request do
   defmacro __using__(_) do
     quote location: :keep do
-      def encode(%__MODULE__{}=req) do
-        Poison.encode(%{"request" => Map.from_struct(req)})
-      end
+      def encode!(%__MODULE__{}=req),
+        do: %{"request" => Map.from_struct(req)}
 
-      def encode!(%__MODULE__{}=req) do
-        Poison.encode!(%{"request" => Map.from_struct(req)})
-      end
-
-      def decode!(json) do
-        Spanner.RequestResponse.decode!(:request, %__MODULE__{}, json)
-      end
-
-      def decode(json) do
-        Spanner.RequestResponse.decode(:request, %__MODULE__{}, json)
-      end
+      def decode!(json),
+        do: Spanner.RequestResponse.decode!(:request, %__MODULE__{}, json)
     end
   end
 end
--- lib/spanner/response.ex
+++ lib/spanner/response.ex
@@ -1,21 +1,11 @@
 defmodule Spanner.Response do
   defmacro __using__(_) do
     quote location: :keep do
-      def encode(%__MODULE__{}=req) do
-        Poison.encode(%{"response" => Map.from_struct(req)})
-      end
+      def encode!(%__MODULE__{}=req),
+        do: %{"response" => Map.from_struct(req)}
 
-      def encode!(%__MODULE__{}=req) do
-        Poison.encode!(%{"response" => Map.from_struct(req)})
-      end
-
-      def decode!(json) do
-        Spanner.RequestResponse.decode!(:response, %__MODULE__{}, json)
-      end
-
-      def decode(json) do
-        Spanner.RequestResponse.decode(:response, %__MODULE__{}, json)
-      end
+      def decode!(json),
+        do: Spanner.RequestResponse.decode!(:response, %__MODULE__{}, json)
     end
   end
 end
