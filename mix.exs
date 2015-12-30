8e4dd5f132ac12e6a3921d19f60c6a766a10d0f5
Christopher Maier <chris@operable.io>
2015-12-30 12:29:55 -0500

    Bump carrier

--- mix.exs
+++ mix.exs
@@ -16,6 +16,6 @@ defmodule Spanner.Mixfile do
 
   defp deps do
     [{:piper, git: "git@github.com:operable/piper", ref: "01a5ff07e9d24b712c5fa71736940ac9b69d1ef8"},
-     {:carrier, git: "git@github.com:operable/carrier", branch: "cm/sync-connection"}]
+     {:carrier, git: "git@github.com:operable/carrier", ref: "fa21dab921ba699d52005fca51b67cc1fa3b3e54"}]
   end
 end
