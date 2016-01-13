f231c0addb53a9cc077103ba9c89098079358b05
Kevin Smith <kevin@operable.io>
2016-01-13 16:16:04 -0500

    Consolidating req/resp marshalling logic
    This commit consolidates req/resp marshalling logic into a single
    module, Spanner.Marshalled, which provides a new primitive
    `defmarshalled` to define structs which are marshalled to/from JSON.
    
    Spanner.Marshalled generates encode/1 and decode!/1 on the using
    module. If the using module defines validate/1 Spanner.Marshalled will
    call it with the results of decoding the struct before returning it from
    decode!/1. This allows callers to implement their own customized struct
    validation logic.

--- lib/spanner/command/reqrep.ex
+++ lib/spanner/command/reqrep.ex
@@ -1,11 +1,39 @@
 defmodule Spanner.Command.Request do
-  defstruct [:room, :requestor, :command, :args, :options, :reply_to]
 
-  use Spanner.Request
+  use Spanner.Marshalled
+
+  defmarshalled [:room, :requestor, :command, :args, :options, :reply_to]
+
+  defp validate(request) do
+    cond do
+      request.room == nil ->
+        {:error, {:empty_field, :room}}
+      request.requestor == nil ->
+        {:error, {:empty_field, :requestor}}
+      request.command == nil ->
+        {:error, {:empty_field, :command}}
+      request.reply_to == nil ->
+        {:error, {:empty_field, :reply_to}}
+      true ->
+        {:ok, request}
+    end
+  end
+
 end
 
 defmodule Spanner.Command.Response do
-  defstruct [:room, :status, :status_message, :body, :bundle, :template]
 
-  use Spanner.Response
+  use Spanner.Marshalled
+
+  defmarshalled [:room, :status, :status_message, :body, :bundle, :template]
+
+  def validate(response) do
+    cond do
+      response.room == nil ->
+        {:error, {:empty_field, :room}}
+      true ->
+        {:ok, response}
+    end
+  end
+
 end
