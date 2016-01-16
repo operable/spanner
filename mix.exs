7177cb21f6a069851726c3609ed2e9320ebe4b58
Kevin Smith <kevin@operable.io>
2016-01-16 15:45:08 -0500

    Add porcelain dep

--- mix.exs
+++ mix.exs
@@ -11,11 +11,13 @@ defmodule Spanner.Mixfile do
   end
 
   def application do
-    [applications: [:logger]]
+    [applications: [:logger,
+                    :porcelain]]
   end
 
   defp deps do
     [{:piper, git: "git@github.com:operable/piper", ref: "888d9df3eeeb16954bf0e68c9083a28baf6c92d5"},
-     {:carrier, git: "git@github.com:operable/carrier", ref: "fa21dab921ba699d52005fca51b67cc1fa3b3e54"}]
+     {:carrier, git: "git@github.com:operable/carrier", ref: "fa21dab921ba699d52005fca51b67cc1fa3b3e54"},
+     {:porcelain, "~> 2.0.1"}]
   end
 end
