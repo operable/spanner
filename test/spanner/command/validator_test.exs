658aba058530f468e92d9996c327520439a3fc59
Kevin Smith <kevin@operable.io>
2016-01-21 14:42:41 -0500

    Validate bundle's config.json
    This commit adds basic XPath-like JSON navigation to Spanner. Navigation
    can optionally also validate the datatype of attribute values allowing
    us to assert invariants about config.json.
    
    The current state validates the basic shape of config.json for foreign
    and Elixir bundles. We can do more to ensure bundle configs are sane but
    this is a good starting point.

---
+++ test/spanner/command/validator_test.exs
@@ -0,0 +1,23 @@
+defmodule Spanner.Command.ValidatorTest do
+
+  alias Spanner.Command.ConfigValidator
+  alias Spanner.Command.ConfigValiationError
+
+  use ExUnit.Case, async: true
+
+  defp get_config(name) do
+    name = name <> ".json"
+    path = Path.join("test/assets/configs", name)
+    data = File.read!(path)
+    Poison.decode!(data)
+  end
+
+  test "validates valid Elixir command config" do
+    assert ConfigValidator.validate(get_config("valid_elixir_config")) == :ok
+  end
+
+  test "validates valid foreign command config" do
+    assert ConfigValidator.validate(get_config("valid_foreign_config")) == :ok
+  end
+
+end
