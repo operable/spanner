e75469720cfc2331f73714d97a97d5ef44309257
Kevin Smith <kevin@operable.io>
2016-01-26 09:28:49 -0500

    Renamed foreign_bundle_extension -> skinny_bundle_extension

--- lib/spanner/spanner.ex
+++ lib/spanner/spanner.ex
@@ -1,10 +1,10 @@
 defmodule Spanner do
   @bundle_extension ".cog"
-  @foreign_bundle_extension ".json"
+  @skinny_bundle_extension ".json"
 
   def bundle_extension(),
     do: @bundle_extension
 
-  def foreign_bundle_extension(),
-    do: @foreign_bundle_extension
+  def skinny_bundle_extension(),
+    do: @skinny_bundle_extension
 end
