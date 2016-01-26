1f520f9eddf8ffae6685f90824a579f5b3a954d0
Latitia Michelle Haskins <latitia@operable.io>
2016-01-26 15:49:06 -0500

    * Add vim swap file to ignore * Add command config loaction as a config location to be pulled from env variable

--- config/config.exs
+++ config/config.exs
@@ -1,3 +1,10 @@
 use Mix.Config
 
+config :logger, :console,
+  metadata: [:module, :line],
+    format: {Adz, :text}
+
+config :spanner, :command_config_root,
+  Path.join([System.get_env("BUNDLE_CONFIG_ROOT"), "command_configs"])
+
 import_config "#{Mix.env}.exs"
