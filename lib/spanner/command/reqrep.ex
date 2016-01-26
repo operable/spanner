1d42618c3563f38c13eacaf33a83fccb48094ce4
Latitia Michelle Haskins <latitia@operable.io>
2016-01-25 23:05:57 -0500

    Add command_config and populate with config values

--- lib/spanner/command/reqrep.ex
+++ lib/spanner/command/reqrep.ex
@@ -1,8 +1,11 @@
 defmodule Spanner.Command.Request do
 
   use Spanner.Marshalled
+  require Logger
 
-  defmarshalled [:room, :requestor, :command, :args, :options, :reply_to, :cog_env]
+  @command_config_root "relay/command_config"
+
+  defmarshalled [:room, :requestor, :command, :args, :options, :command_config, :reply_to, :cog_env]
 
   defp validate(request) do
     cond do
@@ -15,10 +18,30 @@ defmodule Spanner.Command.Request do
       request.reply_to == nil ->
         {:error, {:empty_field, :reply_to}}
       true ->
-        {:ok, request}
+        {:ok, %{request | command_config: populate_config(request)}}
     end
   end
 
+  defp populate_config(request) do
+    case read_config(request) do
+      {:ok, ""} ->
+        Logger.info("The config file for command: #{request.command} is empty")
+        %{}
+      {:ok, config} ->
+        Poison.decode!(config)
+      _ ->
+        Logger.warn("There was a problem accessing the config file for command: #{request.command}")
+        %{}
+    end
+  end
+
+  defp read_config(request) do
+    config_dir = Path.join(Path.rootname(File.cwd!, "cog"), @command_config_root)
+    [ns, _cmd] = String.split(request.command, ":")
+    File.open(Path.join([config_dir, ns, "config.json"]), [:read], fn(file) ->
+      IO.read(file, :all)
+    end)
+  end
 end
 
 defmodule Spanner.Command.Response do
