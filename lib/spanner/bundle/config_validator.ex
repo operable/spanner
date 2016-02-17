f3f8cd7f5d5c6af38e841f0bfbe96e58dea073da
Latitia Michelle Haskins <latitia.haskins@gmail.com>
2016-02-16 23:01:02 -0500

    When installing a bundle, the config.json file should have certain key fields validated to ensure correct installation.
    This change adds more validator logic especially around rules and
    command accepted types and inputs. Helpful errors are displayed in relay
    when a bundle fails these validations checks.
    
    Also, additional tests were added to ensure future changes will
    continue to validate the config.json files as expected.
    
    Fixes #127

--- lib/spanner/bundle/config_validator.ex
+++ lib/spanner/bundle/config_validator.ex
@@ -7,6 +7,8 @@ defmodule Spanner.Bundle.ConfigValidator do
   use Spanner.JsonNavigator
 
   alias Spanner.Bundle.ConfigValidationError, as: ValidationError
+  alias Piper.Permissions.Parser
+  alias Piper.Permissions.Ast
 
   def validate(config) when is_binary(config) do
     case Poison.decode(config) do
@@ -31,7 +33,7 @@ defmodule Spanner.Bundle.ConfigValidator do
     validate!(Poison.decode!(config))
   end
   def validate!(json) when is_map(json) do
-    JsonNavigator.get!(json, ["bundle", {"name", :string}])
+    bundle_name = JsonNavigator.get!(json, ["bundle", {"name", :string}])
     btype = case JsonNavigator.get!(json, ["bundle", {"type", :string}]) do
               type when type in ["foreign", "elixir"] ->
                 type
@@ -39,15 +41,17 @@ defmodule Spanner.Bundle.ConfigValidator do
                 raise ValidationError, field: "type", reason: :wrong_value,
                                        message: "Expected \'foreign\' or \'elixir\' but found \'#{type}\'"
             end
-    JsonNavigator.get!(json, [{"permissions", :array}])
-    JsonNavigator.get!(json, [{"rules", :array}])
     templates = JsonNavigator.get!(json, [{"templates", :array}])
     validate_templates!(templates)
     validate_optional_attributes!(json["bundle"], [{"install", :string}, {"uninstall", :string}])
     commands = JsonNavigator.get!(json, [{"commands", :array}])
+    permissions = JsonNavigator.get!(json, [{"permissions", :array}])
+    rules = JsonNavigator.get!(json, [{"rules", :array}])
+    validate_rules!(rules, permissions, commands, bundle_name)
     validate_commands!(btype, commands)
   end
 
+  # Validate Commands
   defp validate_commands!(_btype, []), do: :ok
   defp validate_commands!("foreign", [cmd|t]) do
     validate_common_command_fields!(cmd)
@@ -60,9 +64,11 @@ defmodule Spanner.Bundle.ConfigValidator do
     JsonNavigator.get!(cmd, [{"module", :string}])
     validate_commands!("elixir", t)
   end
+
   defp validate_common_command_fields!(json) do
     JsonNavigator.get!(json, [{"version", :string}])
-    JsonNavigator.get!(json, [{"options", :array}])
+    options = JsonNavigator.get!(json, [{"options", :array}])
+    validate_options!(options)
     name = JsonNavigator.get!(json, [{"name", :string}])
     enforcing = JsonNavigator.get!(json, [{"enforcing", :boolean}])
     calling_conv = JsonNavigator.get!(json, [{"calling_convention", :string}])
@@ -74,6 +80,61 @@ defmodule Spanner.Bundle.ConfigValidator do
     end
   end
 
+  # Validate Command Options
+  defp validate_options!([]), do: :ok
+  defp validate_options!([option|rem_options]) do
+    JsonNavigator.get!(option, [{"name", :string}])
+    JsonNavigator.get!(option, [{"required", :boolean}])
+    case JsonNavigator.get!(option, [{"type", :string}]) do
+      type when type in ["int", "string", "bool"] ->
+        :ok
+      type ->
+        raise ValidationError, field: "type", reason: :wrong_value,
+                               message: "Expected 'string', 'int', or 'bool' but found '#{type}'"
+    end
+    validate_options!(rem_options)
+  end
+
+  # Validate Rules
+  defp validate_rules!([], [], _, _), do: :ok
+  defp validate_rules!(rules, permissions, commands, bundle_name) when is_list(rules),
+    do: Enum.map(rules, &validate_rules!(&1, permissions, commands, bundle_name))
+
+  defp validate_rules!(rule, permissions, commands, bundle_name) do
+    case Parser.parse(rule) do
+      {:ok, %Ast.Rule{}=expr, rule_perms} ->
+        Enum.map(rule_perms, &verify_rule_permissions!(&1, permissions))
+        verify_rule_commands!(expr.command, Enum.map(commands, &Map.fetch!(&1, "name")), bundle_name)
+      {:error, error} ->
+        raise ValidationError, field: :rules, reason: :bad_format,
+                               message: error
+    end
+  end
+
+  defp verify_rule_permissions!(rule_perms, permissions) do
+    if rule_perms in permissions do
+      :ok
+    else
+      raise ValidationError, field: :rules, reason: :incompatible_values,
+                             message: "The permission #{rule_perms} used in the rule is not in the list of permissions"
+    end
+  end
+
+  defp verify_rule_commands!(rule_command, commands, bundle_name) do
+    [bundle, command] = String.split(rule_command, ":")
+    if bundle_name != bundle do
+      raise ValidationError, field: :rules, reason: :incompatible_values,
+                             message: "The bundle name in the rule #{rule_command} does not match the name in the bundle"
+    end
+    if command in commands do
+      :ok
+    else
+      raise ValidationError, field: :rules, reason: :bad_format,
+                             message: "The command #{rule_command} used in the rule is not in the list of commands"
+    end
+  end
+
+  # Validate Templates
   defp validate_templates!([]), do: :ok
   defp validate_templates!([cmd|t]) do
     JsonNavigator.get!(cmd, [{"name", :string}])
@@ -89,6 +150,7 @@ defmodule Spanner.Bundle.ConfigValidator do
     validate_templates!(t)
   end
 
+  # Validate Optional Attributes (e.g.: install, uninstall)
   defp validate_optional_attributes!(_json, []) do
     :ok
   end
