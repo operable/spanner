f971e4bc65aef56b1372de160c028cc97b7d4d2b
Matthew Peck <matt@operable.io>
2016-02-25 15:36:37 -0500

    bump deps for alias execution

--- mix.exs
+++ mix.exs
@@ -16,7 +16,7 @@ defmodule Spanner.Mixfile do
   end
 
   defp deps do
-    [{:piper, github: "operable/piper", tag: "0.2"},
+    [{:piper, github: "operable/piper", ref: "b78b673c3d6e611e1151d896d4eda393605ef0f7"},
      {:carrier, github: "operable/carrier", tag: "0.2"},
      {:porcelain, "~> 2.0.1"}]
   end
