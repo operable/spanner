7a52570cd77493a0b8bd4f90d74d5dc53b673c54
Matthew Peck <matt@operable.io>
2016-03-11 10:47:31 -0500

    add some simple tests for config parser

---
+++ test/spanner/config_parser_test.exs
@@ -0,0 +1,47 @@
+defmodule Spanner.Config.Parser.Test do
+  use ExUnit.Case, async: true
+  alias Spanner.Config
+
+  test "read_from_string parses valid yaml" do
+    yaml = """
+    ---
+    foo: bar
+    biz:
+    - baz
+    - bliz
+    """
+    expected = %{"foo" => "bar", "biz" => ["baz", "bliz"]}
+
+    {:ok, results} = Config.Parser.read_from_string(yaml)
+    assert results == expected
+  end
+
+  test "read_from_string returns an error with bad yaml" do
+    yaml = """
+    ---
+    ]
+    """
+    {status, _} = Config.Parser.read_from_string(yaml)
+    assert status == :error
+  end
+
+  test "read_from_file parses valid yml" do
+    expected = %{"foo" => [], "bar" => "bar", "baz" => ["biz", "buz"]}
+
+    {:ok, results} = Config.Parser.read_from_file("test/assets/good.yml")
+
+    assert results == expected
+  end
+
+  test "read_from_file returns an error with bad yml" do
+    {status, _} = Config.Parser.read_from_file("test/assets/bad.yml")
+
+    assert status = :error
+  end
+
+  test "read_from_file returns an error with a bad file path" do
+    {status, _} = Config.Parser.read_from_file("foo")
+
+    assert status = :error
+  end
+end
