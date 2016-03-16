9b3413905d8ab29b34bd2b1d951fd3a27e5723ec
Matthew Peck <matt@operable.io>
2016-03-16 14:26:18 -0400

    refactor semantic validator

--- lib/spanner/config/semantic_validator.ex
+++ lib/spanner/config/semantic_validator.ex
@@ -13,36 +13,58 @@ defmodule Spanner.Config.SemanticValidator do
   """
   @spec validate(Map.t) :: :ok | {:error, [{String.t, String.t}]}
   def validate(config) do
-    verify_rules(config)
+    rules       = Map.fetch!(config, "rules")
+    bundle      = Map.fetch!(config, "bundle")
+    commands    = Map.fetch!(config, "commands")
+    permissions = Map.fetch!(config, "permissions")
+
+    errors = rules
+    |> Enum.with_index
+    |> Enum.flat_map(&validate_rule(&1, bundle, commands, permissions))
+
+    case errors do
+      [] ->
+        :ok
+      errors ->
+        {:errors, errors}
+    end
   end
 
-  defp verify_rules(config) do
-    permission_set = MapSet.new(config["permissions"])
+  defp validate_rule({rule, index}, bundle, commands, permissions) do
+    {:ok, %Ast.Rule{}=expr, rule_permissions} = Parser.parse(rule)
+    [rule_bundle, rule_command] = String.split(expr.command, ":", parts: 2)
 
-    Enum.with_index(config["rules"])
-    |> Enum.reduce([], fn({rule, index}, acc) ->
-      {:ok, %Ast.Rule{}=expr, rule_perms} = Parser.parse(rule)
-      [bundle, command] = String.split(expr.command, ":", parts: 2)
+    errors = [validate_bundle(rule_bundle, bundle["name"]),
+              validate_command(rule_command, commands),
+              validate_permissions(rule_permissions, permissions)]
 
-      if config["bundle"]["name"] != bundle do
-        acc = [{"The bundle name '#{bundle}' does not match the name in the bundle", "#/rules/#{index}"}  | acc]
-      end
-      if not(command) in Enum.map(config["commands"], &Map.fetch!(&1, "name")) do
-        acc = [{"The command '#{expr.command}' is not in the list of commands", "#/rules/#{index}"}  | acc]
-      end
+    errors
+    |> List.flatten
+    |> Enum.map(&{&1, "#/rules/#{index}"})
+  end
 
-      case MapSet.difference(MapSet.new(rule_perms), permission_set) |> MapSet.to_list do
-        [] ->
-          acc
-        bad_perms ->
-          Enum.map(bad_perms, fn(bad_perm) ->
-            {"The permission '#{bad_perm}' is not in the list of permissions.", "#/rules/#{index}"}
-          end) ++ acc
-      end
+  defp validate_bundle(bundle, bundle),
+    do: []
+  defp validate_bundle(rule_bundle, bundle),
+    do: ["The bundle name '#{bundle}' does not match the name in the bundle, '#{rule_bundle}'."]
+
+  defp validate_command(rule_command, commands) do
+    command_names = Enum.map(commands, &Map.fetch!(&1, "name"))
+
+    case rule_command in command_names do
+      true ->
+        []
+      false ->
+        ["The command '#{rule_command}' is not in the list of commands"]
+    end
+  end
+
+  defp validate_permissions(rule_permissions, permissions) do
+    missing_permissions = MapSet.difference(MapSet.new(rule_permissions), MapSet.new(permissions)) |> MapSet.to_list
+
+    Enum.map(missing_permissions, fn permission ->
+      "The permission '#{permission}' is not in the list of permissions."
     end)
-    |> (fn ([]) -> :ok
-           (errors) -> {:error, errors}
-        end).()
   end
 
 end
