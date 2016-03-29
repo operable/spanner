81a47641ddd6c5cd9269951d0761a89a86d7f36a
Matthew Peck <matt@operable.io>
2016-03-29 15:08:14 -0400

    improve Spanner.Config.config_file?/1 logic

--- lib/spanner/config.ex
+++ lib/spanner/config.ex
@@ -1,10 +1,7 @@
 defmodule Spanner.Config do
   @config_extensions [".yaml", ".yml", ".json"]
-  @config_file "config.yaml"
+  @config_file "config"
   @dynamic_config_file "config.yaml"
-
-  def file_name(),
-    do: @config_file
 
   def dynamic_file_name(),
     do: @dynamic_config_file
@@ -13,7 +10,7 @@ defmodule Spanner.Config do
     do: @config_extensions
 
   def config_file?(filename) do
-    String.ends_with?(filename, config_extensions)
+    String.ends_with?(filename, Enum.map(config_extensions, &("#{@config_file}#{&1}")))
   end
 
   def validate(config) do
