c88d527c38484d2ceb921d145f1337c25cf0c4e5
Operable Engineering <engineering@operable.io>
2017-02-22 14:19:03 -0500

    Updated app version

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Spanner.Mixfile do
 
   def project do
     [app: :spanner,
-     version: "1.0.0-beta.3",
+     version: "1.0.0-beta.4",
      elixir: "~> 1.3.1",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
