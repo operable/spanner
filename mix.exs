50568b96629a8f82dca80c7b1320ea8a8147cd74
Operable Engineering <engineering@operable.io>
2017-02-23 11:04:35 -0500

    Locking dependencies for release

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Spanner.Mixfile do
 
   def project do
     [app: :spanner,
-     version: "1.0.0-beta.4",
+     version: "1.0.0",
      elixir: "~> 1.3.1",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
@@ -16,7 +16,7 @@ defmodule Spanner.Mixfile do
   end
 
   defp deps do
-    [{:piper, github: "operable/piper"},
+    [{:piper, github: "operable/piper", branch: "v1.0.0"},
      {:yaml_elixir, "~> 1.2"},
      {:ex_json_schema, "~> 0.5"}]
   end
