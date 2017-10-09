0fc0cca91097ecdd341f12f57f74abb28d139b84
Dave Long <dlong@cagedata.com>
2017-10-08 22:12:51 -0400

    Upgrades piper

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Spanner.Mixfile do
 
   def project do
     [app: :spanner,
-     version: "1.1.0",
+     version: "1.1.1",
      elixir: "~> 1.3.1",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
